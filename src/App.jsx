db3338e34efe28452bb116b5cd17b9e7b8c1a106
AhmadGh18 <=>
2025-01-22 22:33:10 +0200

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "./components/Navbar";
-import Banner from "./components/Banner";
 import Contactus from "./components/ContactUs";
 import ClientsReview from "./components/ClientsReview";
 
